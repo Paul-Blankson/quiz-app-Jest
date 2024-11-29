8c764b6cd1fbb88bc56b41be9e7e7a0ba5b64db9
Paul Blankson <paul.blankson@amalitech.com>
2024-11-29 10:10:26 +0000

    refactor: utilized quiz services to display card details

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
-import { SmallCardComponent } from "../small-card/small-card.component";
+import { SmallCardComponent } from '../small-card/small-card.component';
+import { QuizServiceService } from '../../service/quiz-service.service';
+import { HttpClient } from '@angular/common/http';
 
+interface QuizData {
+  title: string;
+  icon: string;
+  questions: Question[];
+}
+
+interface Question {
+  question: string;
+  options: string[];
+  answer: string;
+}
 @Component({
   selector: 'app-main-content',
   imports: [SmallCardComponent],
@@ -8,5 +21,21 @@ import { SmallCardComponent } from "../small-card/small-card.component";
   styleUrl: './main-content.component.css'
 })
 export class MainContentComponent {
+  card: QuizData[]=[]
+
+  constructor(private quizService: QuizServiceService, private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.quizService.getQuizData().subscribe({
+        next: (data) => {
+          this.card = data.quizzes;
+            console.log('Quiz Data:', this.card);
+        },
+        error: (err) => {
+            console.error('Error fetching quiz data:', err);
+        },
+    });
+}
 
 }
+
