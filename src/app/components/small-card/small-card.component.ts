7621d88fd66a8d946a77cbe3cb9e0934da24627d
Paul Blankson <paul.blankson@amalitech.com>
2024-11-29 10:43:53 +0000

    fix: added a method get the background color of the of the small card

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
+
+@Input() cardIcon: string = ''
+@Input() cardTitle: string = ''
+@Input() cardIconBackground: string = ''
+
+getCardIconBackgroundColor(): string {
+  switch (this.cardTitle) {
+    case 'HTML':
+      return '#FFF1E9';
+    case 'CSS':
+      return '#E0FDEF';
+    case 'JavaScript':
+      return '#EBF0FF';
+    case 'Accessibility':
+      return '#F6E7FF';
+    default:
+      return 'transparent';
+  }
+}
 
 }
