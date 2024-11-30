753dac23bbeca249ddfb3e7e77763126b79da6a9
Paul Blankson <paul.blankson@amalitech.com>
2024-11-30 19:15:05 +0000

    refactor: refactored to add an Output

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,30 +1,31 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { QuizData } from '../../types';
 @Component({
   selector: 'app-small-card',
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
+  @Input() card: QuizData[] = [];
+  @Output() cardSelect = new EventEmitter<QuizData>();
 
-@Input() cardIcon: string = ''
-@Input() cardTitle: string = ''
-@Input() cardIconBackground: string = ''
-
-getCardIconBackgroundColor(): string {
-  switch (this.cardTitle) {
-    case 'HTML':
-      return '#FFF1E9';
-    case 'CSS':
-      return '#E0FDEF';
-    case 'JavaScript':
-      return '#EBF0FF';
-    case 'Accessibility':
-      return '#F6E7FF';
-    default:
-      return 'transparent';
+  getIconBackgroundColor(title: string): string {
+    switch (title) {
+      case 'HTML':
+        return '#FFF1E9';
+      case 'CSS':
+        return '#E0FDEF';
+      case 'JavaScript':
+        return '#EBF0FF';
+      case 'Accessibility':
+        return '#F6E7FF';
+      default:
+        return 'transparent';
+    }
   }
-}
 
+  onCardClick(card: QuizData) {
+    this.cardSelect.emit(card);
+  }
 }
