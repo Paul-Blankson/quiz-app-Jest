06c97dcb6700afdf88c18f8275eea6ea8a1f864e
Paul Blankson <paul.blankson@amalitech.com>
2024-11-30 19:21:06 +0000

    refactor: removed interface to types folder and imported it

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,41 +1,29 @@
 import { Component } from '@angular/core';
 import { SmallCardComponent } from '../small-card/small-card.component';
 import { QuizServiceService } from '../../service/quiz-service.service';
-import { HttpClient } from '@angular/common/http';
+import { QuizData } from '../../types';
 
-interface QuizData {
-  title: string;
-  icon: string;
-  questions: Question[];
-}
-
-interface Question {
-  question: string;
-  options: string[];
-  answer: string;
-}
 @Component({
   selector: 'app-main-content',
   imports: [SmallCardComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.css'
+  styleUrl: './main-content.component.css',
 })
 export class MainContentComponent {
-  card: QuizData[]=[]
+  card: QuizData[] = [];
 
-  constructor(private quizService: QuizServiceService, private http:HttpClient) { }
+  constructor(
+    private quizService: QuizServiceService
+  ) {}
 
   ngOnInit(): void {
     this.quizService.getQuizData().subscribe({
-        next: (data) => {
-          this.card = data.quizzes;
-            console.log('Quiz Data:', this.card);
-        },
-        error: (err) => {
-            console.error('Error fetching quiz data:', err);
-        },
+      next: (data) => {
+        this.card = data.quizzes;
+      },
+      error: (err) => {
+        console.error('Error fetching quiz data:', err);
+      },
     });
+  }
 }
-
-}
-
