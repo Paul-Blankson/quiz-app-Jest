b8181fa219a2657979040da251188daad67c8447
Paul Blankson <paul.blankson@amalitech.com>
2024-12-02 15:49:17 +0000

    feat: implemented dynamic display of header logo based on card seclection

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { QuizServiceService } from '../../service/quiz-service.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,39 @@ import { Component, Input } from '@angular/core';
 })
 export class HeaderComponent {
 
- @Input() headerLogo: string = '';
- @Input() headerIconBackground: string = '';
- @Input() headerTitle: string = '';
- @Input() isFirstPage: boolean = true;
+  @Input() headerIconBackground: string = '';
+  @Input() headerTitle: string = '';
+  @Input() isFirstPage: boolean = true;
+
+  quizService = inject(QuizServiceService);
+
+  getHeaderIcon(): string {
+    switch (this.quizService.selectedSubjectTitle) {
+      case 'HTML':
+        return 'images/icon-html.svg';
+      case 'CSS':
+        return 'images/icon-css.svg';
+      case 'JavaScript':
+        return 'images/icon-js.svg';
+      case 'Accessibility':
+        return 'images/icon-accessibility.svg';
+      default:
+        return '';
+    }
+  }
+
+  getHeaderIconBackground(): string {
+    switch (this.quizService.selectedSubjectTitle) {
+      case 'HTML':
+        return '#FFF1E9';
+      case 'CSS':
+        return '#E0FDEF';
+      case 'JavaScript':
+        return '#EBF0FF';
+      case 'Accessibility':
+        return '#F6E7FF';
+      default:
+        return 'transparent';
+    }
+  }
 }
