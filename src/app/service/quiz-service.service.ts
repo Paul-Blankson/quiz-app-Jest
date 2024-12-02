b4694ac315ca479b21f46909fdf612e26ffd4100
Paul Blankson <paul.blankson@amalitech.com>
2024-12-02 15:55:16 +0000

    fix: removed unused interface

--- src/app/service/quiz-service.service.ts
+++ src/app/service/quiz-service.service.ts
@@ -2,25 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-interface QuizData {
-  title: string;
-  icon: string;
-  questions: Question[];
-}
-
-interface Question {
-  question: string;
-  options: string[];
-  answer: string;
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class QuizServiceService {
 
   quizUrl = '/data/data.json'
-
 
   constructor(private http: HttpClient) {}
 
