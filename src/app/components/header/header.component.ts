32376344971f560608594d651f26091b76621883
Paul Blankson <paul.blankson@amalitech.com>
2024-12-02 11:47:36 +0000

    refactor: ustilize component communication with @Input

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+ @Input() headerLogo: string = '';
+ @Input() headerIconBackground: string = '';
+ @Input() headerTitle: string = '';
+ @Input() isFirstPage: boolean = true;
 }
