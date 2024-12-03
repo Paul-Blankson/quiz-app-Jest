bc2d46ed166acec85ffaee5ebfd6263913ea3a34
Paul Blankson <paul.blankson@amalitech.com>
2024-12-03 23:48:00 +0000

    feat: added setter and getter methods to get isFirstPage value

--- src/app/service/quiz-service.service.ts
+++ src/app/service/quiz-service.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizServiceService {
-
-  quizUrl = '/data/data.json'
+  quizUrl = '/data/data.json';
 
   constructor(private http: HttpClient) {}
 
-  getQuizData(): Observable<any> {
-    return this.http.get(this.quizUrl);
-  }
-
-  selectedSubjectTitle = '';
+  private selectedSubjectTitle = '';
 
   setSelectedSubjectTitle(title: string): void {
     this.selectedSubjectTitle = title;
   }
+
   getSelectedSubjectTitle(): string {
     return this.selectedSubjectTitle;
+  }
+
+  private isFirstPageSubject = new BehaviorSubject<boolean>(true);
+
+  setIsFirstPage(isFirstPage: boolean): void {
+    this.isFirstPageSubject.next(isFirstPage);
+  }
+
+  getIsFirstPage(): Observable<boolean> {
+    return this.isFirstPageSubject.asObservable();
+  }
+
+  getQuizData(): Observable<any> {
+    return this.http.get(this.quizUrl);
   }
 }
