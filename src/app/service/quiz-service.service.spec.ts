9c007debac67494d0543c3616789dfa11704bd1f
Paul Blankson <paul.blankson@amalitech.com>
2024-12-14 18:34:09 +0000

    feat(QuizService): add test for fetching quiz data correctly

--- src/app/service/quiz-service.service.spec.ts
+++ src/app/service/quiz-service.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 import { QuizServiceService } from './quiz-service.service';
+import * as quizData from '../../../public/data/data.json';
+import { QuizData } from '../types';
 
 describe('QuizServiceService', () => {
   let service: QuizServiceService;
@@ -42,5 +44,27 @@ describe('QuizServiceService', () => {
     });
   });
 
-  
+  it('should fetch quiz data correctly', () => {
+    const mockQuizData: QuizData[] = quizData.quizzes;
+
+    service.getQuizData().subscribe(data => {
+      expect(data).toBeTruthy();
+      expect(data.length).toBeGreaterThan(0);
+
+      expect(data[0].title).toBeDefined();
+      expect(data[0].icon).toBeDefined();
+      expect(data[0].questions).toBeDefined();
+
+      const firstQuestion = data[0].questions[0];
+      expect(firstQuestion.question).toBe("What does HTML stand for?");
+      expect(firstQuestion.options.length).toBe(4);
+      expect(firstQuestion.answer).toBe("Hyper Text Markup Language");
+    });
+
+    const req = httpMock.expectOne('/data/data.json');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockQuizData);
+  });
+
 });
