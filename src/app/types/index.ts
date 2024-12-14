6a8c5dcc57ad5ddfe912cc72c8ef402103418874
Paul Blankson <paul.blankson@amalitech.com>
2024-12-14 18:29:45 +0000

    refactor: type the questions property in the QuizData to be an array of objects

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,11 +1,10 @@
-export interface QuizData {
-  title: string;
-  icon: string;
-  questions: [];
-}
-
 export interface Question {
   question: string;
   options: string[];
   answer: string;
+}
+export interface QuizData {
+  title: string;
+  icon: string;
+  questions: Question[];
 }
